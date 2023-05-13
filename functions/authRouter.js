93be5564513bc4720c8058ae1eb185a9d9d08dbf
levosay <ryuuzaki44l@gmail.com>
2023-05-13 21:15:00 +0300

    remeve middlewaree

--- functions/authRouter.js
+++ functions/authRouter.js
@@ -2,15 +2,15 @@ const Router = require('express')
 const controller = require('./authController')
 const router = new Router
 const { check } = require('express-validator')
-const authMiddleware = require('./middlewaree/authMiddleware')
+// const authMiddleware = require('./middlewaree/authMiddleware')
 
 router.post('/signup', [
   check('username', 'Имя пользователя не может быть пустым').notEmpty(),
   check('password', 'Пароль должен быть больше 6 символов').isLength({ min: 6 })
 ], controller.signUp)
 router.post('/signin', controller.signIn)
-router.get('/users', authMiddleware, controller.getUsers)
-router.get('/users/:id', authMiddleware, controller.getUser)
-router.get('/me', authMiddleware, controller.getMe)
+router.get('/users', controller.getUsers)
+router.get('/users/:id', controller.getUser)
+router.get('/me', controller.getMe)
 
 module.exports = router
--- functions/questionRouter.js
+++ functions/questionRouter.js
@@ -1,11 +1,11 @@
 const Router = require('express')
 const controller = require('./questionController')
 const router = new Router
-const authMiddleware = require('./middlewaree/authMiddleware')
+// const authMiddleware = require('./middlewaree/authMiddleware')
 
 // router.get('/questions', controller.getQuestions)
-router.get('/questions/:id', authMiddleware, controller.getQuestions)
-router.post('/questions', authMiddleware,controller.postQuestionCreate)
-router.post('/answers', authMiddleware, controller.postAnswers )
+router.get('/questions/:id', controller.getQuestions)
+router.post('/questions',controller.postQuestionCreate)
+router.post('/answers', controller.postAnswers )
 
 module.exports = router
