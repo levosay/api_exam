3ddc6daa69a0b11ac498415ccbd492521a3c25f8
levosay <ryuuzaki44l@gmail.com>
2023-05-13 17:27:49 +0300

    netlify config

---
+++ functions/api.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv")
+dotenv.config()
+const express = require('express')
+const mongoose = require('mongoose')
+const serverless = require('serverless-http')
+const authRouter = require('./authRouter')
+const questionRouter = require('./questionRouter')
+const MONGO_URL = process.env.MONGO_URL || ''
+const BASE_NETLIFY = process.env.BASE_NETLIFY || ''
+const app = express()
+
+app.use(express.json())
+app.use(`${BASE_NETLIFY}/auth`, authRouter)
+app.use(`${BASE_NETLIFY}/exam`, questionRouter)
+
+mongoose.connect(MONGO_URL)
+
+module.exports.handler = serverless(app);
